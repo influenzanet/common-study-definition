c349856332d7b3443e79c0d166637c63b4be7b43
Clément Turbelin <clement.turbelin@gmail.com>
2022-11-10 13:04:42 +0100

    add trans_text help (for helpGroup content)

--- src/tools/text.ts
+++ src/tools/text.ts
@@ -5,9 +5,15 @@ import { _T } from "../studies/common/languages";
 /**
  * Text Componenet based on translated text
  */
-export const transTextComponent = (id: string, ref:string, opts: Omit<TextProps, "content">): ItemComponent => {
+export const transTextComponent = (id: string, ref:string, opts?: Omit<TextProps, "content">): ItemComponent => {
     return textComponent({
         content: _T(id, ref),
         ...opts
     });
 }
+
+export const trans_text = (id:string, ref:string) => {
+    return {
+        content : _T(id, ref)
+    }
+};
