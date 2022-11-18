ea9222f1103876c17fd6e7b2bd5c146f3220a6f5
Clément Turbelin <clement.turbelin@gmail.com>
2022-11-18 14:32:45 +0100

    add common in fr-be

--- src/studies/common/languages/fr-be/index.ts
+++ src/studies/common/languages/fr-be/index.ts
@@ -3,9 +3,11 @@ import { LanguageHelpers } from "../languageHelpers";
 import intake from "./intake.json";
 import weekly from "./weekly.json";
 import vaccination from "./vaccination.json";
+import common from "./common.json";
 
 const languageId = "fr-be";
 
 LanguageHelpers.addLanguage(languageId, intake);
 LanguageHelpers.addLanguage(languageId, weekly);
 LanguageHelpers.addLanguage(languageId, vaccination);
+LanguageHelpers.addLanguage(languageId, common);
