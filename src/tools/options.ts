ed67ee66f141fe5e35c5f435c0fa649ac2c6be2b
Clément Turbelin <clement.turbelin@gmail.com>
2022-11-18 12:04:11 +0100

    add option_def() tool, more generic

--- src/tools/options.ts
+++ src/tools/options.ts
@@ -1,5 +1,8 @@
 import { OptionDef } from "case-editor-tools/surveys/types";
 
+
+export const default_input_option_style = [{ key: 'className', value: 'w-100' }] as const;
+
 /**
  * Helper to create a simple option component
  * @param key
@@ -8,7 +11,6 @@ import { OptionDef } from "case-editor-tools/surveys/types";
  * @returns
  */
 export function as_option(key:string, content: Map<string,string>): OptionDef {
-
     return {
         role: 'option',
         key: key,
@@ -20,8 +22,37 @@ export function as_input_option(key:string, content: Map<string,string>, descrip
     return {
         key: key,
         role: "input",
-        style: [{ key: 'className', value: 'w-100' }],
+        style: Array.from(default_input_option_style),
         content: content,
         description: description
     };
+}
+
+interface OptionDefOpts extends Omit<OptionDef, "key" | "content" | "role"> {
+    role?: string; // Override with optional property, will be set as 'option' by default
+    defaultStyle?: boolean; // For input type can apply the default style
+}
+
+/**
+ * Generic option definition generator
+ * By default will generate a simple option
+ * @param key
+ * @param content
+ * @param opts
+ * @returns
+ */
+export const option_def = (key:string, content: Map<string,string>, opts?: OptionDefOpts): OptionDef => {
+    const role = opts?.role ?? 'option';
+
+    const o = {
+        key: key,
+        role: role,
+        content: content,
+        ...opts,
+    };
+
+    if(role == 'input' && opts?.defaultStyle && !opts?.style) {
+        o.style = Array.from(default_input_option_style);
+    }
+    return o;
 }
