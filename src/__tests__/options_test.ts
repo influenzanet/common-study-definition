51fc00a1405557065ff9b4bc96fee3ed8fe5da3f
Clément Turbelin <clement.turbelin@gmail.com>
2022-11-18 13:54:35 +0100

    add OptionList helper class and test case (deps updated)

---
+++ src/__tests__/options_test.ts
@@ -0,0 +1,111 @@
+import { OptionDef } from "case-editor-tools/surveys/types";
+import { format } from "util";
+import { _T } from "../studies/common/languages";
+import { as_option,OptionList } from "../tools";
+
+const o = (key:string) => {
+    return as_option(key, _T(key, key));
+}
+
+// Index of a key in an option list
+const index = (key:string, oo: OptionDef[]) => {
+    return oo.findIndex(o => {
+        return o.key === key;
+    });
+}
+
+const new_list = (...keys:string[]) => {
+    const oo = keys.map(k => o(k));
+    return new OptionList(oo);
+};
+
+test("optionlist_values", ()=>{
+
+    const kk = ['A','B'];
+
+    const list = new_list(...kk);
+    const oo = list.values();
+
+    kk.forEach((key, index) => {
+        const o = oo[index];
+        expect(o.key).toBe(key);
+    });
+});
+
+const keylist = (oo: OptionDef[]) => oo.map(o => o.key).join(',');
+
+const test_insert_at = (from:string[], at:number, key:string) => {
+    const l = new_list(...from);
+    l.insertAt(at, o(key));
+    const oo = l.values();
+
+    test(format('Inserting %s in %s at %d => %s', key, from.join(','), at, keylist(oo)), () => {
+        expect(index(key, oo)).toBe(at);
+    });
+}
+
+describe("optionlist_insert", ()=> {
+
+    const kk = ['A','B'];
+
+    test_insert_at(kk, 0, 'C');
+    test_insert_at(kk, 1, 'C');
+
+    const k2 = ['a','b','c','d'];
+    test_insert_at(k2, 0, 'C');
+    test_insert_at(k2, 1, 'C');
+    test_insert_at(k2, 2, 'C');
+    test_insert_at(k2, 3, 'C');
+
+});
+
+const test_insert_before = (from:string[], before:string, key:string) => {
+    const l = new_list(...from);
+    const at = index(before, l.values());
+    l.insertBeforeKey(before, o(key));
+    const oo = l.values();
+    test(format('Inserting %s in %s before %s => %s', key, from.join(','), before, keylist(oo)), ()=>{
+        expect(index(key, oo)).toBe(at);
+        expect(index(before, oo)).toBe(at + 1);
+    });
+}
+
+describe("optionlist_insert_before", ()=> {
+    const kk = ['a','b','c','d'];
+    test_insert_before(kk, 'a', 'X');
+    test_insert_before(kk, 'b', 'X');
+    test_insert_before(kk, 'c', 'X');
+    test_insert_before(kk, 'd', 'X');
+    test("unknown_key", ()=> {
+        expect(()=>{
+                    test_insert_before(kk, 'e', 'X')
+                }
+            ).toThrowError(/is not found in list/);
+    });
+});
+
+
+const test_insert_after = (from:string[], after:string, key:string) => {
+    const l = new_list(...from);
+    const at = index(after, l.values());
+    l.insertAfterKey(after, o(key));
+    const oo = l.values();
+    test(format('Inserting %s in %s after %s => %s', key, from.join(','), after, keylist(oo)), ()=>{
+        expect(index(key, oo)).toBe(at + 1); // Added key is next index
+        expect(index(after, oo)).toBe(at); // Target key still in same place
+    });
+}
+
+describe("optionlist_insert_after", ()=> {
+    const kk = ['a','b','c','d'];
+    test_insert_after(kk, 'a', 'X');
+    test_insert_after(kk, 'b', 'X');
+    test_insert_after(kk, 'c', 'X');
+    test_insert_after(kk, 'd', 'X');
+    test("unknown_key", ()=> {
+        expect(()=>{
+            test_insert_after(kk, 'e', 'X')
+                }
+            ).toThrowError(/is not found in list/);
+    });
+});
--- src/tools/options.ts
+++ src/tools/options.ts
@@ -1,4 +1,5 @@
 import { OptionDef } from "case-editor-tools/surveys/types";
+import { _T } from "../studies/common/languages";
 
 
 export const default_input_option_style = [{ key: 'className', value: 'w-100' }] as const;
@@ -33,6 +34,17 @@ interface OptionDefOpts extends Omit<OptionDef, "key" | "content" | "role"> {
     defaultStyle?: boolean; // For input type can apply the default style
 }
 
+export const option_input_other = (key:string, content: Map<string,string>, trans:string, opts?:OptionDefOpts) => {
+    const defaultStyle = opts?.defaultStyle ?? true;
+    const o: OptionDefOpts = {
+        ...opts,
+        role: 'input',
+        defaultStyle: defaultStyle,
+        description: _T(trans, "Describe here (optional)", "common.other_describe_here")
+    };
+    return option_def(key, content, o )
+}
+
 /**
  * Generic option definition generator
  * By default will generate a simple option
@@ -55,4 +67,66 @@ export const option_def = (key:string, content: Map<string,string>, opts?: Optio
         o.style = Array.from(default_input_option_style);
     }
     return o;
+}
+
+/**
+ * Manipulate
+ */
+export class OptionList {
+    options: OptionDef[]
+
+    constructor(options:OptionDef[]) {
+        this.options = options;
+    }
+
+    /**
+     *
+     * @param key key to find and insert element after this element
+     * @param oo list of options to add
+     * @returns OptionList fluent interface
+     */
+    insertAfterKey(key:string, ...oo:OptionDef[]) {
+        const index = this.indexOf(key);
+        if(index < 0) {
+            throw new Error("Option key '"+key+"' is not found in list");
+        }
+        this.insertAt(index + 1, ...oo);
+        return this;
+    }
+
+    insertBeforeKey(key:string, ...oo:OptionDef[]) {
+        const index = this.indexOf(key);
+        if(index < 0) {
+            throw new Error("Option key '"+key+"' is not found in list");
+        }
+        var i = index;
+        if(i < 0) {
+            i = 0;
+        }
+        this.insertAt(i, ...oo);
+        return this;
+    }
+
+    indexOf(key:string):number {
+        return this.options.findIndex((o)=>{
+            return o.key == key;
+        });
+    }
+
+    insertAt(index: number, ...oo:OptionDef[]) {
+        this.options.splice(index, 0, ...oo);
+        return this;
+    }
+
+    without(...keys:string[]) {
+        this.options = this.options.filter((o)=> !keys.includes(o.key));
+    }
+
+    /**
+     * Get options after update
+     * @returns Get
+     */
+    values(): OptionDef[] {
+        return this.options;
+    }
 }
