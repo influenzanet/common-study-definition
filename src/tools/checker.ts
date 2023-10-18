4de4754ed5a61b11e9a36e80e1ab9db55d507d24
Clément Turbelin <clement.turbelin@gmail.com>
2023-10-18 14:36:30 +0200

    more generic checks, check component properties (must be EpressionArg)

--- src/tools/checker.ts
+++ src/tools/checker.ts
@@ -1,8 +1,8 @@
 import { Study } from "case-editor-tools/types/study";
-import { Expression, isExpression, isItemGroupComponent, isSurveyGroupItem, ItemComponent, Survey, SurveyGroupItem, SurveyItem } from "survey-engine/data_types";
+import { ComponentProperties, ExpressionArg, Expression, isExpression, isItemGroupComponent, isSurveyGroupItem, ItemComponent, Survey, SurveyGroupItem, SurveyItem } from "survey-engine/data_types";
 import { Problem, ProblemTypes } from "../types";
-import { ItemQuestion } from "./items";
 import { SurveyDefinition } from "case-editor-tools/surveys/types";
+import { isExpressionArg, isValidExpressionArg } from "./expressions";
 
 interface CheckResult {
     checks: string[]
@@ -25,7 +25,7 @@ export class SurveyChecker {
     }
 
     check():CheckResult {
-        this.checkEmptyItems(this.survey.surveyDefinition);
+        this.checkSurveyItem(this.survey.surveyDefinition);
         this.checks.push('empty_element');
         return {
             'problems': this.problems,
@@ -33,51 +33,96 @@ export class SurveyChecker {
         };
     }
 
-    raise(type:ProblemTypes, itemKey: string, path?: string) {
-        console.log(" - " + itemKey + ' '+ path + " = " + type);
-        this.problems.push({
-            type: type,
-            item: itemKey,
-            path: path
-        });
+    raise(type:ProblemTypes, itemKey: string, path?: string, message?:string) {
+        console.log(" - " + itemKey + ' '+ path + " = " + type + (message ? " : "+message :''));
+        const p: Problem = {type: type, item: itemKey};
+        if(path) {
+            p.path = path;
+        }
+        if(message) {
+            p.message = message;
+        }
+        this.problems.push(p);
     }
 
-    checkEmptyItems(item: SurveyItem) {
-        //console.log('check ' + item.key);
+    checkSurveyItem(item: SurveyItem) {
         if('items' in item) { // Cannot use isSurveyGroupItem because it check length
-            if(item.items) {
+            if(Array.isArray(item.items)) {
                 if(item.items.length == 0) {
                     console.log("empty item " + item.key);
                     this.raise(ProblemTypes.empty_element, item.key, 'items');
                 } else {
-                    item.items.forEach(i => this.checkEmptyItems(i));
+                    item.items.forEach(i => this.checkSurveyItem(i));
                 }
             }
         } else {
             if(item.components) {
-                this.checkEmptyComponents(item.key, 'components:', item.components );
+                this.checkComponents(item.key, 'components:', item.components );
             }
         }
     }
 
-    checkEmptyComponents(itemKey: string, path: string, comp: ItemComponent) {
+    /**
+     * Check the component tree
+     * @param itemKey
+     * @param path
+     * @param comp
+     */
+    checkComponents(itemKey: string, path: string, comp: ItemComponent) {
         if('items' in comp && comp.items) {
             if(comp.items.length == 0) {
-                console.log("empty componenent " + itemKey + ' ' + path);
+                console.log("empty component " + itemKey + ' ' + path);
                 this.raise(ProblemTypes.empty_element, itemKey, );
             } else {
                 comp.items.forEach( (item, index) =>  {
                     const p = path + '/' + (item.key ? item.key : '[' + index + ']@' + item.role);
-                    this.checkEmptyComponents(itemKey, p, item);
+                    this.checkComponents(itemKey, p, item);
                 });
             }
         }
+        this.checkComponent(itemKey, path, comp);
+    }
+
+    /**
+     * Check the component itself
+     * @param itemKey
+     * @param path
+     * @param comp
+     */
+    checkComponent(itemKey: string, path: string, comp: ItemComponent) {
         if(comp.content && comp.content.length == 0) {
             this.raise(ProblemTypes.empty_element, itemKey, path +':content');
         }
         if(comp.description && comp.description.length == 0) {
             this.raise(ProblemTypes.empty_element, itemKey, path +':description');
         }
+        if(comp.properties) {
+            this.checkComponentProperties(itemKey, path+':properties', comp.properties);
+        }
+
+    }
+
+    checkComponentProperties(itemKey:string, path:string, props: ComponentProperties) {
+        const check = (name: keyof ComponentProperties) => {
+            if (!(name in props)){
+                return;
+            }
+            const v = props[name];
+            if(typeof(v) === "undefined") {
+                return;
+            }
+            if(isExpressionArg(v)) {
+                if(!isValidExpressionArg(v)) {
+                    this.raise(ProblemTypes.wrong_type, itemKey, path+':'+name, 'Property must be ExpressionArg');
+                }
+            } else {
+                this.raise(ProblemTypes.wrong_type, itemKey, path+':'+name, 'Property must be ExpressionArg');
+            }
+        }
+        check('dateInputMode');
+        check('max');
+        check('min');
+        check('stepSize');
     }
 
 }
@@ -102,7 +147,7 @@ export class StudyChecker {
      }
 
      checkSurvey(surveyDef: SurveyDefinition):CheckResult {
-        // Need to get the built survey, caution cannot use surveyDef.getSurvey() because it rebuilds the survey and generate duplicate keys 
+        // Need to get the built survey, caution cannot use surveyDef.getSurvey() because it rebuilds the survey and generate duplicate keys
         const survey = surveyDef.editor.getSurvey();
         const checker = new SurveyChecker(survey);
         return checker.check();
--- src/types/check.ts
+++ src/types/check.ts
@@ -1,9 +1,11 @@
 export enum ProblemTypes {
     empty_element = "empty_element",
+    wrong_type = 'wrong_type'
 }
 
 export interface Problem {
     type: ProblemTypes;
     item: string; // Key of item
     path?: string; // Subpath in item
+    message?: string;
 }
