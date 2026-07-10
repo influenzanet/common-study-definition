f93bf970b0be4024e5860b01092c951fa179328d
gf-isi <104016673+gf-isi@users.noreply.github.com>
2026-07-10 15:38:23 +0200

    added it helper

---
+++ src/studies/heatwave/languages/it.ts
@@ -0,0 +1,7 @@
+import { LanguageHelpers } from "../../common/languages/languageHelpers";
+
+import heatwave from "./it.json";
+
+const languageId = "it";
+
+LanguageHelpers.addLanguage(languageId, heatwave, "heatwave/it");
