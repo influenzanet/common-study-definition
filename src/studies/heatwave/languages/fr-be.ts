7bf2fdc336fd5ad81ecadadf21e180efa552e664
gf-isi <104016673+gf-isi@users.noreply.github.com>
2026-07-15 14:49:34 +0200

    added language helpers for Belgium

---
+++ src/studies/heatwave/languages/fr-be.ts
@@ -0,0 +1,7 @@
+import { LanguageHelpers } from "../../common/languages/languageHelpers";
+
+import heatwave from "./fr-be.json";
+
+const languageId = "fr-be";
+
+LanguageHelpers.addLanguage(languageId, heatwave, "heatwave/fr-be");
---
+++ src/studies/heatwave/languages/nl-be.ts
@@ -0,0 +1,7 @@
+import { LanguageHelpers } from "../../common/languages/languageHelpers";
+
+import heatwave from "./nl-be.json";
+
+const languageId = "nl-be";
+
+LanguageHelpers.addLanguage(languageId, heatwave, "heatwave/nl-be");
